455d1593486b2e3a13fd586e95b16941e55414a2
daneedev <dan@danee.dev>
2026-03-13 22:05:47 +0100

    Fix import paths for components in router and views

--- src/views/web10s.vue
+++ src/views/web10s.vue
@@ -1,50 +1,45 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import ExhibitCard from "./components/exhibitCard.vue";
+import ExhibitCard from "../components/exhibitCard.vue";
 </script>
 
 <template>
-    <nav>
-        <header>
-            <p>Web 2010. léta</p>
-            <p>Interaktivní modely a exponáty</p>
-            <section>
-                <RouterLink to="/">Domov</RouterLink>
-                <RouterLink to="/web90">90. léta</RouterLink>
-                <RouterLink to="/web2000">2000. léta</RouterLink>
-                <RouterLink to="/web2020">Současnost</RouterLink>
-            </section>
-        </header>
-    </nav>
-    <main>
-        <article>
-            <section class="heroSection">
-                <h1>Vítejte na webu 2010. let</h1>
-            </section>
-            <section>
-                
-            </section>
-        </article>
-    </main>
+  <nav>
+    <header>
+      <p>Web 2010. léta</p>
+      <p>Interaktivní modely a exponáty</p>
+      <section>
+        <RouterLink to="/">Domov</RouterLink>
+        <RouterLink to="/web90">90. léta</RouterLink>
+        <RouterLink to="/web2000">2000. léta</RouterLink>
+        <RouterLink to="/web2020">Současnost</RouterLink>
+      </section>
+    </header>
+  </nav>
+  <main>
+    <article>
+      <section class="heroSection">
+        <h1>Vítejte na webu 2010. let</h1>
+      </section>
+      <section></section>
+    </article>
+  </main>
 </template>
 
 <style scoped>
-    :root {
-        --page-bg: #F5F6F7;
-        --primary-color: #3498DB;
-        --text-color: #7F8C8D;
-        --primary-dark: #2980B9;
-        --header-text: #FFFFFF;
+:root {
+  --page-bg: #f5f6f7;
+  --primary-color: #3498db;
+  --text-color: #7f8c8d;
+  --primary-dark: #2980b9;
+  --header-text: #ffffff;
+}
 
-    }
-
-    main {
-        background-color: var(--page-bg);
-    }
-    nav {
-        background-color: #2C3E50;
-        color: #FFFFFF;
-    }
-
-
+main {
+  background-color: var(--page-bg);
+}
+nav {
+  background-color: #2c3e50;
+  color: #ffffff;
+}
 </style>
