3a315cfa43a8f1e374bb522bbdaf368980ea96f6
daneedev <dan@danee.dev>
2026-03-13 11:29:24 +0100

    feat: add Font Awesome icons and update App layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from "./components/Button.vue";
+</script>
 
 <template>
-  <h1>Ahoj svete</h1>
+  <section class="hero-section">
+    <h1 class="hero-text">Vítejte v muzeu internetu</h1>
+    <Button label="Klikni mě" />
+  </section>
 </template>
+
+<style>
+.hero-text {
+  font-size: 3rem;
+  font-family: "Archivo Black", sans-serif;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/main.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount("#app");
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fas, far, fab);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
