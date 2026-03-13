26a4450f32e1310e0a8288af5456ad88e909e016
SebastianBroz <broz.se.2023@skola.ssps.cz>
2026-03-13 11:05:33 +0100

    Start page declaration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import Start from "./components/Start.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Start />
   </StrictMode>,
 );
